0734397e9ef7e0bfc90194e34e4ed1350f90a0ea
nimnapathum <pathumeshan123@gmail.com>
2025-05-20 00:46:17 +0530

    [HMS-006] FEAT : Changes on App and configures

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 
 function App() {
 
   return (
     <div>
-          <Home />
+          <Home/>
     </div>
   )
 }
