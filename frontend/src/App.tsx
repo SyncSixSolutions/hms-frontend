f9d2c492799ec88733b55ca9ec7ad0f2799e3c26
nimnapathum <pathumeshan123@gmail.com>
2025-05-24 22:59:27 +0530

    FIX : app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
-import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 
 function App() {
 
   return (
     <div>
-          {/* <Home /> */}
-          <Dashboard/>
+          <Home />
     </div>
   )
 }
