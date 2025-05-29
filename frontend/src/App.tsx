cd1a5c7fea11fb6f180811142b09834f696b71ad
nimnapathum <pathumeshan123@gmail.com>
2025-05-29 16:50:30 +0530

    [HMS-7] add router element to signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShowRooms from './pages/Client/Booking/ShowRooms';
 import Home from './pages/Home';
+import SignUp from "./pages/Auth/SignUp";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/rooms" element={<ShowRooms />} />
+        <Route path="/signup" element={<SignUp/>} ></Route>
       </Routes>
     </Router>
   );
