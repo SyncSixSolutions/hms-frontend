905a47207d6a9655c715fbf9cdde1a2279077d08
nadun_hasalanka <nadun202327@gmail.com>
2025-05-30 09:35:44 +0530

    [HMS-15]created the AddVehicle from the Admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Home from './pages/Home';
 import VehicleListing from './pages/Client/Vehicle/VehicleListing';
+import AddVehicle from './pages/Admin/CRUDS/Vehicle/AddVehicle';
 
 function App() {
 
   return (
     <div>
-          <Home />
+          {/* <Home /> */}
+          <AddVehicle />
         {/* <VehicleListing /> */}
     </div>
   )
---
+++ frontend/src/pages/Admin/CRUDS/Vehicle/AddVehicle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddVehicle = () => {
+  return (
+    <div>
+      <h1>hello from the vehicle admin</h1>
+    </div>
+  )
+}
+
+export default AddVehicle
