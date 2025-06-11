39f9c4485a65b06d7d459de749a553c8aaeddef2
Pawan Weerasuriya <uwpawan@gmail.com>
2025-06-11 23:42:15 +0530

    [feature/HMS-13] app.tsx routings added.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShowRooms from './pages/Client/Booking/ShowRooms';
 import Home from './pages/Home';
 import SignUp from "./pages/Auth/SignUp";
-import VehicleListing from './pages/Client/Vehicle/VehicleListing';
-import ShowTherapyItems from './pages/Client/Therapy/ShowTherapyItems';
+import ViewTherapiesPage from "./pages/Admin/CRUDS/Therapy/ViewTherapies";
+import AddTherapyPage from "./pages/Admin/CRUDS/Therapy/AddTherapy";
+import EditTherapyPage from "./pages/Admin/CRUDS/Therapy/EditTherapy";
 
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/rooms" element={<ShowRooms />} />
         <Route path="/signup" element={<SignUp/>} ></Route>
+        <Route path="/therapies" element={<ViewTherapiesPage />} />
+        <Route path="/addtherapy" element={<AddTherapyPage />} />
+        <Route path="/edittherapy" element={<EditTherapyPage />} />
       </Routes>
     </Router>
   );
